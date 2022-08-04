c20c48e2cbbb94df8aacb25fd457dc9f05c7f965
Boula Akladyous <akladyous@gmail.com>
2022-08-04 17:51:10 -0400

    updated resgistration controller

--- app/controllers/users/reset_passwords_controller.rb
+++ app/controllers/users/reset_passwords_controller.rb
@@ -1,6 +1,8 @@
 class Users::ResetPasswordsController < Users::UsersController
     # before_action :method
 
+    attr_accessor :error_message
+
     def new
         redirect_to root_path, error: 'You are already signed in!' and return if current_user
     end
@@ -11,14 +13,17 @@ class Users::ResetPasswordsController < Users::UsersController
             Users::PasswordMailer.with(user: @user).reset_password.deliver_now
             redirect_to root_path, notice: 'email found' and return
         else
-            render :new, error: 'invalid email' and return
+            self.error_message = 'Please enter a valid email address'
+            flash[:error] = self.error_message
+            render :new and return
         end
     end
 
     def edit
+        self.error_message = 'Token is expired. Please try again'
         @user = User.find_signed!(controller_params[:token], purpose: 'reset_password')
         rescue ActiveSupport::MessageVerifier::InvalidSignature
-            redirect_to reset_user_password_path, error: 'token is expired'
+            redirect_to reset_user_password_path, error: self.error_message
     end
 
     def update
