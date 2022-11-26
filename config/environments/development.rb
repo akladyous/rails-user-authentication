3edd3a61c2ba23dcab0f0a22df9b258fe022d0cc
Boula Akladyous <akladyous@gmail.com>
2022-11-26 14:47:22 -0500

    dev mailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,6 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = false
     config.action_mailer.perform_caching = false
     config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:email_provider, :host) }
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.perform_caching = false
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
